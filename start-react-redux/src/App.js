e4c479b11499e7f15615f9a1caed340da0d53959
mitsuhiro0602 <0hornet0@gmail.com>
2021-05-08 08:55:43 +0900

    create class component and functiuonal component

--- start-react-redux/src/App.js
+++ start-react-redux/src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
-function App() {
-  const greeting = "Hi, Tom!";
-  const dom = <h1 className="foo">{greeting}</h1>;
+// function App() {
+//   const greeting = "Hi, Tom!";
+//   const dom = <h1 className="foo">{greeting}</h1>;
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log("I am clicked")}} />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log("I am clicked")}} />
-    </React.Fragment>
-  );
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
