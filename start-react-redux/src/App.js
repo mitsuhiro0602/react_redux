8781f9bed0cd32f414f80ff6b024d226a4a81977
mitsuhiro0602 <0hornet0@gmail.com>
2021-05-08 08:40:52 +0900

    jsx syntax

--- start-react-redux/src/App.js
+++ start-react-redux/src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 
 function App() {
+  const greeting = "Hi, Tom!";
+  const dom = <h1 className="foo">{greeting}</h1>;
   return (
-    <div>
-        <h1>
-          Hello, world
-        </h1>
-    </div>
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clicked")}} />
+    </React.Fragment>
   );
 }
 
