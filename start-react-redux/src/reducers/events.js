ffd88af930a48cb72e231267601084c13c0c7733
mitsuhiro0602 <0hornet0@gmail.com>
2021-05-09 17:27:01 +0900

    implement eventsShow

--- start-react-redux/src/reducers/events.js
+++ start-react-redux/src/reducers/events.js
@@ -2,11 +2,21 @@
 import _ from 'lodash'
 import { 
   READ_EVENTS,
-  DELETE_EVENT
+  READ_EVENT,
+  CREATE_EVENT,
+  DELETE_EVENT,
+  UPDATE_EVENT,
 } from '../actions'
 
 export default (events = {}, action) => {
   switch (action.type) {
+    case READ_EVENT:
+    case UPDATE_EVENT:
+    case CREATE_EVENT:
+      const data = action.response.data
+      // console.log(action.response.data)
+      // {id: 6, token: "token123", title: "Let's have an event 6!", body: "This is the body for event 6."
+      return {...events, [data.id]: data }
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id')
     case DELETE_EVENT:
